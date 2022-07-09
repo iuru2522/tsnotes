1cdade07d20bc77b11694146314eade8f4ffa2e6
Iurii Manastyrskyi <ternoboy@gmail.com>
2022-07-09 16:15:18 -0400

    generics.ts added

---
+++ gererics.ts
@@ -0,0 +1,12 @@
+//GENERICS
+//generics are one of the main tools in the toolbox box for creating reusable
+//components is generics, that is being able to create a component that can work
+// over a variety of types rather than a single one.
+//this allows users to consume there components and use their own types.
+
+
+//HELLO WORLD OF GENERICS
+
+function indentity(arg: number): number {
+    return arg;
+}
