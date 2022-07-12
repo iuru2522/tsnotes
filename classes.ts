54ec94e72410d4c9c84fd13a4066ec0f5f664973
Iurii Manastyrskyi <ternoboy@gmail.com>
2022-07-12 16:55:14 -0400

    tsconfig --strictPropertyInitialization setup

--- classes.ts
+++ classes.ts
@@ -1,48 +1,41 @@
 //CLASSES
 //Class mermbers
-class Point{}
+// class daPoint{}
 
 //Fields
-class thePoint {
-    x: number;
-    y: number;
-    z: number;
-}
+// class thePoint {
+//     x: number;
+//     y: number;
+//     z: number;
+// }
 
-const pt = new thePoint();
+// const pt = new daPoint();
 
-pt.x = 0;
-pt.y = 0;
-pt.z = 0;
+// pt.x = 0;
+// pt.y = 0;
+// pt.z = 0;
 
-class newPoint {
-    x = 0;
-    y = 0;
-    z = 0;
-}
-const thePt = new newPoint();
-console.log(`${pt.x}, ${pt.y}`);
+// class daPoint {
+//     x = 0;
+//     y = 0;
+//     z = 0;
+// }
+// const thePt = new daPoint();
+// console.log(`${pt.x}, ${pt.y}`);
 
 // const PT = new thePoint();
 // //Type 'string' is not assignable to type 'number'.
 // pt.x = "0";
 
-//--strictPropertyInitialization
-
-class UserAccount {
+class BadGreeter {
+    //to see this error, you need to setup tsconfig file. ""--strictPropertyInitialization" to true
+     //Property 'name' has no initializer and is not definitely assigned in the constructor.
     name: string;
-    accountType = "user";
+ 
   
-    email: string;
-    address: string | undefined;
-  
-    constructor(name: string) {
-      this.name = name;
-      // Note that this.email is not set
-    }
-}
+  }
 
-class Good {
+class NiceGreeter {
     name: string;
     constructor(){
         this.name = "yo"
